1abf1736adcfa357568c239c63cef897c1bd087b
mingo99 <2839131656@qq.com>
2022-11-20 10:28:30 +0800

    Verify quantizable MobileNetv3 is available.

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from torchinfo import summary
 from coco_names import COCO_INSTANCE_CATEGORY_NAMES as coco_names
 from torchvision.models.quantization import *
 from quantization import *
+from PIL import Image
 
 def get_model(device):
     # load the model 
@@ -15,16 +16,29 @@ def get_model(device):
     return model
 
 def get_quant_model():
-    weights=torchvision.models.detection.SSDLite320_MobileNet_V3_Large_Weights.DEFAULT
+    weights = torchvision.models.detection.SSDLite320_MobileNet_V3_Large_Weights.DEFAULT
     model = qssdlite320_mobilenet_v3_large(pretrained=True,weights=weights,quantize=True)
     return model.eval()
+
+def get_mobilenetv3(device):
+    weights=torchvision.models.quantization.MobileNet_V3_Large_QuantizedWeights.DEFAULT
+    model = torchvision.models.quantization.mobilenet_v3_large(pretrained=True, weights=weights,quantize=True)
+    return model.eval().to(device)
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # ssdlite = get_model(device)
     # # summary(ssdlite,(1,3,320,320), device=device)
     # print(ssdlite)
-    qmnetv3 = get_quant_model(device)
-    print(qmnetv3.state_dict().keys())
+    # qmnetv3 = get_quant_model(device)
+    # print(qmnetv3.state_dict().keys())
     # summary(ssdlite,(1,3,320,320), device=device)
     # print(qmnetv3)
+
+    # Test quant_mobilenetv3
+    transform = torchvision.transforms.ToTensor()
+    model = get_mobilenetv3(device)
+    image = Image.open('input/image_1.jpg')
+    image = transform(image).to(device)
+    image = image.unsqueeze(0)
+    print(model(image))
