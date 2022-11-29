fa50847bca0093b50bf515442e08ab7bbd9fe350
mingo99 <2839131656@qq.com>
2022-11-29 18:52:28 +0800

    Debug.

--- datasets/coco.py
+++ datasets/coco.py
@@ -1,3 +1,4 @@
+import random
 import torch
 import torchvision.transforms as transforms
 from torchvision.datasets.coco import CocoDetection
@@ -9,7 +10,7 @@ transform = transforms.Compose([
     transforms.Resize((320,320))
 ])
 
-coco_root = "../../data/coco/"
+coco_root = "../../../data/coco/"
 coco_img_train = coco_root+"images/train2014/"
 coco_img_val = coco_root+"images/val2014/"
 coco_ann_train = coco_root+"annotations/instances_train2014.json"
@@ -49,9 +50,22 @@ def get_coco_datasets(batch_size,train=True):
 def get_coco_calibrate_datasets(batch_size):
     dataset = CocoDetection(coco_img_val,coco_ann_val,transform=transform)
     print(dataset.coco.getAnnIds())
+    path = dataset.coco.loadImgs(id)[0]["file_name"]
 
 
 if __name__ == "__main__":
     dataset = CocoDetection(coco_img_val,coco_ann_val,transform=transform)
-    print(dataset.coco.loadImgs([581913,581929]))
+    calib_ds_len = 0
+    ds_len = 0
+    for catId in dataset.coco.getCatIds():
+        imgIds = dataset.coco.getImgIds(catIds=catId)
+        idx_max = (len(imgIds)+19)//20
+        # print(len(imgIds))
+        imgIds_part = random.sample(imgIds,idx_max)
+        # print(len(imgIds_part))
+        ds_len += len(imgIds)
+        calib_ds_len += len(imgIds_part)
+    print(imgIds)
+    print(ds_len)
+    print(calib_ds_len)
     # print(len(dataset.ids))
