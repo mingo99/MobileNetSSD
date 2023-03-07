4389a137b511bef6abf3b079c67b87762144c3c5
mingo99 <2839131656@qq.com>
2023-03-07 15:18:02 +0800

    Modify scripts on server.

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ date
 module load anaconda/2021.05
 source activate pytorch
 export PYTHONUNBUFFERED=1
-torchrun --nproc_per_node=8 train.py -e=$1 -b=$2 -n=$3 -lr=$4 --resume=$5 -ds=/dev/shm/cocofb/ -y=2017 --eval_freq=5
+torchrun --nproc_per_node=$1 train.py -e=$2 -b=$3 -n=$4 -lr=$5 --resume=$6 -ds=/dev/shm/cocofb/ -y=2023 --eval_freq=5
