59212f6ab3d090819766393198d85e41741709cc
mingo99 <2839131656@qq.com>
2023-03-12 17:54:13 +0800

    Modify parameters of model to get better results.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ run_server:
 	sbatch --gpus=${GPUS} run.sh ${GPUS} 660 24 8 0.15 ""
 
 run_gpuc128:
-	sbatch --gpus=8 -p gpu_c128 run.sh 8 660 24 8 0.15 ""
+	sbatch --gpus=8 -p gpu_c128 run.sh 8 220 16 8 0.01 ""
 
 run_resume:
 	sbatch --gpus=8 -p gpu_c128 run.sh 8 660 24 8 0.15 "./checkpoint/normal/checkpoint.pth"
@@ -11,7 +11,7 @@ run_mm:
 	sbatch -N 2 --gres=gpu:8 --qos=gpugpu run.sh 220 24 16 0.15 ""
 
 run_pc:
-	python train.py -e=330 -b=24 -n=4 -lr=0.05 --aspect_ratio_group_factor=0 -y=2023 --eval_freq=5
+	python train.py -e=220 -b=24 -n=4 -lr=0.15 --aspect_ratio_group_factor=0 -y=2023 --eval_freq=5
 
 run_test:
 	python train.py -y=2017 --eval_freq=5 --test-only True --resume "./checkpoint/normal/best.pth"
--- analy_model.py
+++ analy_model.py
@@ -1,16 +1,18 @@
 import torch
 import torchvision
+from torchinfo import summary
 
 ssdlite = torchvision.models.detection.ssdlite320_mobilenet_v3_large()
-wh_pairs = ssdlite.anchor_generator._wh_pairs
-print(ssdlite.anchor_generator.scales)
-print(ssdlite.anchor_generator.steps)
-ssdlite = ssdlite.eval().cuda()
-dummy_input = torch.rand((1, 3, 320, 320), device="cuda")
-features1 = ssdlite.backbone(dummy_input)
-_ = ssdlite(dummy_input)
+ssd = torchvision.models.detection.ssd300_vgg16()
+ssdlite = ssdlite.cuda()
+ssd = ssd.cuda()
+# dummy_input = torch.rand((1, 3, 320, 320), device="cuda")
+# features1 = ssdlite.backbone(dummy_input)
+# _ = ssdlite(dummy_input)
 # features2 = ssd.backbone(dummy_input)
 # for key in features1.keys():
 #     print(features1[key].shape)
 # for anchor in ssdlite.anchors[0]:
 #     print(anchor)
+summary(ssdlite,(1, 3, 320, 320))
+# summary(ssd,(1, 3, 300, 300))
